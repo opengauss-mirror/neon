2738988f45029a131b91344ee76c46568fc9a323
wangjingyuan8 <1577039175@qq.com>
2026-06-15 20:26:06 +0800

    修改预取初始化参数

--- control_plane/src/endpoint.rs
+++ control_plane/src/endpoint.rs
@@ -469,10 +469,7 @@ impl Endpoint {
         conf.append("hot_standby", "on");
         conf.append("shared_buffers", "1GB");
         conf.append("full_page_writes", "off");
-        // Postgres defaults to effective_io_concurrency=1, which does not exercise the pageserver's
-        // batching logic.  Set this to 2 so that we exercise the code a bit without letting
-        // individual tests do a lot of concurrent work on underpowered test machines
-        conf.append("effective_io_concurrency", "2");
+        conf.append("effective_io_concurrency", "8");
         conf.append("fsync", "off");
         conf.append("max_connections", "100");
         conf.append("wal_level", "logical");
